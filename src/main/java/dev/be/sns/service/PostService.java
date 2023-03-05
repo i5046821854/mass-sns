1d7b1594436542b45f8746e799562c8b67665927
machinaa <o5046821854@gmail.com>
2023-03-05 23:56:30 +0900

    feat: modifying post

--- src/main/java/dev/be/sns/service/PostService.java
+++ src/main/java/dev/be/sns/service/PostService.java
@@ -4,6 +4,7 @@ import dev.be.sns.exception.ErrorCode;
 import dev.be.sns.exception.SnsApplicationException;
 import dev.be.sns.model.Entity.PostEntity;
 import dev.be.sns.model.Entity.UserEntity;
+import dev.be.sns.model.Post;
 import dev.be.sns.repository.PostEntityRepository;
 import dev.be.sns.repository.UserEntityRepository;
 import lombok.AllArgsConstructor;
@@ -25,9 +26,20 @@ public class PostService {
 
     }
 
-    public void modify(String title, String body, String userName, int postId){
+    public Post modify(String title, String body, String userName, int postId){
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(()->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", userName)));
 
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not found", postId)));
+
+        if(postEntity.getUser() != userEntity){
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission", userName));
+        }
+
+        postEntity.setTitle(title);
+        postEntity.setBody(body);
+
+        return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
 }
