50f3a5c06f3ad0c624aa5152aa712d57f7632130
machinaa <o5046821854@gmail.com>
2023-03-06 03:39:28 +0900

    feat: post deletion with authentication

--- src/main/java/dev/be/sns/service/PostService.java
+++ src/main/java/dev/be/sns/service/PostService.java
@@ -42,4 +42,18 @@ public class PostService {
 
         return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
+
+    public void delete(String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(()->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", userName)));
+
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not found", postId)));
+
+        if(postEntity.getUser() != userEntity){
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission", userName));
+        }
+
+        postEntityRepository.delete(postEntity);
+    }
 }
