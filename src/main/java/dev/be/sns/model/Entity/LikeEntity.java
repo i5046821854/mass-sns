879a8b90de647b2632e0d7937c309085dab5a91f
machinaa <o5046821854@gmail.com>
2023-03-07 17:05:08 +0900

    feat: create entity for like

---
+++ src/main/java/dev/be/sns/model/Entity/LikeEntity.java
@@ -0,0 +1,61 @@
+package dev.be.sns.model.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "\"like\"")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE \"like\" SET deleted_at = NOW() whrere id = ?")  //delete 될때 실행할 것
+@Where(clause = "deleted_at is NULL")  //쿼리 수행시 조건
+public class LikeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt()
+    {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt()
+    {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static LikeEntity of(UserEntity userEntity, PostEntity postEntity)
+    {
+        LikeEntity likeEntity = new LikeEntity();
+        likeEntity.setUser(userEntity);
+        likeEntity.setPost(postEntity);
+        likeEntity.setUser(userEntity);
+        return likeEntity;
+    }
+}
---
+++ src/main/java/dev/be/sns/repository/LikeEntityRepository.java
@@ -0,0 +1,21 @@
+package dev.be.sns.repository;
+
+import dev.be.sns.model.Entity.LikeEntity;
+import dev.be.sns.model.Entity.PostEntity;
+import dev.be.sns.model.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer> {
+
+    Optional<LikeEntity> findByUserAndPost(UserEntity user, PostEntity post);
+
+
+    @Query(value = "SELECT COUNT(e.id) from LikeEntity e WHERE e.post = :post")
+    Integer countByPost(@Param("post") PostEntity post);
+
+}
