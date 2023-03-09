66465ae382b371eeafb109ef9f30afb20fa99622
machinaa <o5046821854@gmail.com>
2023-03-10 01:33:54 +0900

    feat: use cache(redis) when login

--- src/main/java/dev/be/sns/service/UserService.java
+++ src/main/java/dev/be/sns/service/UserService.java
@@ -6,6 +6,7 @@ import dev.be.sns.model.Alarm;
 import dev.be.sns.model.Entity.UserEntity;
 import dev.be.sns.model.User;
 import dev.be.sns.repository.AlarmEntityRepository;
+import dev.be.sns.repository.UserCacheRepository;
 import dev.be.sns.repository.UserEntityRepository;
 import dev.be.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ public class UserService {
     private final BCryptPasswordEncoder encoder;
     private final UserEntityRepository userEntityRepository;
     private final AlarmEntityRepository alarmEntityRepository;
+    private final UserCacheRepository userCacheRepository;
 
     @Value("${jwt.secret-key}")
     private String secretKey;
@@ -34,10 +36,10 @@ public class UserService {
 
 
     public User loadUserByUserName(String userName) {
-        return userEntityRepository.findByUserName(userName)
-                .map(User::fromEntity)
-                .orElseThrow(()->
-                        new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found", userName)));
+        return userCacheRepository.getUser(userName).orElseGet(()->
+            userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(()->
+                        new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found", userName)))
+        );
     }
 
     @Transactional
@@ -54,9 +56,11 @@ public class UserService {
 
     public String login(String userName, String password) {
         //회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded)", userName)));
+        User user = loadUserByUserName(userName);
+        userCacheRepository.setUser(user);
+
         //비밀번호 체크
-        if(encoder.matches(password, userEntity.getPassword())){
+        if(encoder.matches(password, user.getPassword())){
         //if(!userEntity.getPassword().equals(password)) {
             throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
