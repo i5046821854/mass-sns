34f73414382fd48a4283d49033e69547bba1bd74
machinaa <o5046821854@gmail.com>
2023-03-13 23:49:50 +0900

    refactor: add logger to check if the event has been consumed

--- src/main/java/dev/be/sns/consumer/AlarmConsumer.java
+++ src/main/java/dev/be/sns/consumer/AlarmConsumer.java
@@ -3,10 +3,12 @@ package dev.be.sns.consumer;
 import dev.be.sns.model.event.AlarmEvent;
 import dev.be.sns.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AlarmConsumer {
@@ -15,7 +17,8 @@ public class AlarmConsumer {
 
     @KafkaListener(topics = "${spring.kafka.topic.alarm}")
     public void consumeAlarm(AlarmEvent event, Acknowledgment ack){
-            alarmService.send(event.getAlarmType(), event.getAlarmArgs(), event.getReceiveUserId());
-            ack.acknowledge();
+        log.info("Consume the event {}", event);
+        alarmService.send(event.getAlarmType(), event.getAlarmArgs(), event.getReceiveUserId());
+        ack.acknowledge();
     }
 }
